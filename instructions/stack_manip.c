a7b2d2838985b65a976e86232c7d37519b1ffb95
Deepesh Choudhary <dcdeepesh7@gmail.com>
2023-09-26 14:48:31 +0530

    Fix begin() & end() implementation

--- instructions/stack_manip.c
+++ instructions/stack_manip.c
@@ -18,12 +18,15 @@ int get_variable(Program* program, char* var_name, BOOL address) {
             return address ? i : fc->variables[i]->value;
         }
     }
-    // If we're in a begin...end block, look in callee's FC too
+    // If we're in a begin...end block, look in callee's FC too, for params
     if (program->callee_fc != NULL) {
         fc = program->callee_fc;
         for (int i = 0; i < fc->total_variables; i++) {
-            if (strcmp(fc->variables[i]->name, var_name) == 0) {
-                return address ? i : fc->variables[i]->value;
+            if (strcmp(fc->variables[i]->name, var_name) == 0
+                && fc->variables[i]->parameter) {
+                // An offset of 1000 to distinguish local variables
+                // from callee's parameters
+                return address ? (i + 1000) : fc->variables[i]->value;
             }
         }
     }
@@ -42,6 +45,11 @@ int get_variable(Program* program, char* var_name, BOOL address) {
     fc->variables[new_var_address] = variable;
     fc->total_variables++;
 
+    if (program->callee_fc != NULL) {
+        // An offset of 1000 to distinguish local variables
+        // from callee's parameters
+        new_var_address += 1000;
+    }
     return address ? new_var_address : 0;
 }
 
@@ -57,11 +65,24 @@ void pop(Program* program, char* args) {
     stack_pop_value(program->stack);
 }
 
+// TODO uses stack_base
+// Helper function for assign()
+// Handles both callee's and current FCs
+void set_variable(Program* program, int address, int value) {
+    // An offset of 1000 to distinguish local variables from callee's parameters
+    if (address >= 1000) {
+        program->callee_fc->variables[address - 1000]->value = value;
+    } else {
+        FunctionContext* current_fc =
+            program->stack->values[program->stack_base - 2].fc_ptr;
+        current_fc->variables[address]->value = value;
+    }
+}
+
 void assign(Program* program, char* args) {
     int value = stack_pop_value(program->stack);
     int address = stack_pop_value(program->stack);
-    FunctionContext* current_fc = program->stack->values[program->stack_base - 2].fc_ptr;
-    current_fc->variables[address]->value = value;
+    set_variable(program, address, value);
 }
 
 void copy(Program* program, char* args) {
