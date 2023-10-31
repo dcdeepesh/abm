43c698d78fb63e2bcee616682aa4dac2193d8193
Deepesh Choudhary <dcdeepesh7@gmail.com>
2023-10-31 17:54:17 +0530

    Complete .int instruction

--- src/instructions/v2.c
+++ src/instructions/v2.c
@@ -1,4 +1,20 @@
+#include <string.h>
 #include "program.h"
+#include "util.h"
+
+void create_global_variable(Program* program, char* var_name) {
+    Variable* variable = new(Variable);
+    variable->name = var_name;
+    variable->value = 0;
+
+    program->global_variables[program->total_global_variables] = variable;
+    program->total_global_variables++;
+}
 
 void int_(Program* program, char* args) {
+    char *var_name = strtok(args, " ");
+    while (var_name) {
+        create_global_variable(program, var_name);
+        var_name = strtok(NULL, " ");
+    }
 }
